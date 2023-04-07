daea2b97c21cc2a9a2046b9892669f48c11e4365
nikabe10 <belamaric.nika@gmail.com>
2023-04-07 17:56:49 +0200

    Cleared folders

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import "./App.css";
-import WeatherSearch from "./WeatherSearch";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Weather Search</h1>
-        <WeatherSearch />
-      </header>
-    </div>
-  );
+  return <div className="App">Weather App</div>;
 }
 
 export default App;
