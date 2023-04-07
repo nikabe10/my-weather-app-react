594d34b750d99cfa9910b8d1fa2dec1f46580228
nikabe10 <belamaric.nika@gmail.com>
2023-04-07 18:08:05 +0200

    Added GitHub repo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This code was created by Nika Župan and it's{" "}
+        <a
+          href="https://github.com/nikabe10/my-weather-app-react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
