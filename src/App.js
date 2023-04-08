441976a38427ffcce31d9ccf09dde53654cc924f
nikabe10 <belamaric.nika@gmail.com>
2023-04-08 16:55:28 +0200

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        {" "}
-        This code was created by Nika Župan and it's{" "}
-        <a
-          href="https://github.com/nikabe10/my-weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          {" "}
+          This code was created by Nika Župan and it's{" "}
+          <a
+            href="https://github.com/nikabe10/my-weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
