e0f3bbfcdd0ce47af38d73b9a20380e034c3f919
nikabe10 <belamaric.nika@gmail.com>
2023-04-13 17:32:14 +0200

    fixed sizing of icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -18,7 +18,7 @@ export default function WeatherInfo(props) {
         <div className="col-4">
           <div className="clearfix">
             <div className="float-left">
-              <WeatherIcon code={props.data.icon} size={52} />
+              <WeatherIcon code={props.data.icon} size={64} />
             </div>
           </div>
         </div>
