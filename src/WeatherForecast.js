9c622d57ebc0cbe378a67e2a4e62a2ef94518afe
nikabe10 <belamaric.nika@gmail.com>
2023-04-14 10:17:10 +0200

    aded API call

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,19 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "e0a5a97de9a0b7a951e9d154a8f9bad8";
+  let latitude = props.coordinates.lat;
+  let longitude = props.coordinates.lat;
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}
+`;
+  axios.get(apiURL).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
