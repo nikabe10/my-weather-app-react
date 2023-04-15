232c7889d942622f3c498481078435e5fc4ad67c
nikabe10 <belamaric.nika@gmail.com>
2023-04-15 16:19:08 +0200

    aded all for forecast day

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,18 +2,37 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
   return (
     <div className="WeatherForecastDay">
-      <div className="WeatherForecast-day">{props.data.dt}</div>
+      <div className="WeatherForecast-day">{day()}</div>
 
       <WeatherIcon code={props.data.weather[0].icon} size={36} />
 
       <div className="WeatherForecast-temperature">
         <span className="WeatherForecast-temperature-max">
-          {Math.round(props.data.temp.max)}°
+          {maxTemperature()}
         </span>
         <span className="WeatherForecast-temperature-min">
-          {Math.round(props.data.temp.min)}°
+          {minTemperature()}
         </span>
       </div>
     </div>
